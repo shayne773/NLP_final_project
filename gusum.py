ee2fb414e7d5e8ed31c694a83b2add7553b7c76a
Kai-Hsuan Chan <shayne773@gmail.com>
2024-11-27 22:32:08 -0500

    update readme and added td-idf feature for gusum algorithm

--- gusum.py
+++ gusum.py
@@ -4,6 +4,7 @@ from nltk.tag import pos_tag
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 class Graph:
 
@@ -15,6 +16,7 @@ class Graph:
         self.vertices = []
         self.edges = []
         self.centralities = []
+        self.tfidf_dict = {}
 
         #initialize vertices
         for position, sentence in enumerate(self.sentences):
@@ -35,12 +37,29 @@ class Graph:
         for i, vertex in enumerate(self.vertices):
             vertex.centrality = self.centralities[i]
 
+
+        #initializie tf-idf dictionary
+        vectorizer = TfidfVectorizer()
+        tfidf_matrix = vectorizer.fit_transform(self.sentences)
+        feature_names = vectorizer.get_feature_names_out()
+        idf_scores = vectorizer.idf_
+        self.tfidf_dict = dict(zip(feature_names, idf_scores))
+        #calculate tf-idf scores for vertices
+        for vertex in self.vertices:
+            vertex.calculate_tfidf_score(self.tfidf_dict)
+
     
     #outputs the top three scored sentences in the document (will be changed later)
     def summarize(self) -> list:
         
         #each element is a tuple that contains the original sentence and its respective score
-        scores = [(vertex.original_sentence, vertex.score_vertex(self.max_sentence_length, self.num_sentences)) for vertex in self.vertices]
+
+        #original features for scoring
+        #scores = [(vertex.original_sentence, vertex.score_vertex(self.max_sentence_length, self.num_sentences)) for vertex in self.vertices]
+
+        #------enable this for testing added tf-idf feature-------
+        scores = [(vertex.original_sentence, vertex.score_vertex_2(self.max_sentence_length, self.num_sentences)) for vertex in self.vertices]
+        #---------------------------------------------------------
         sorted_scores = sorted(scores, key = lambda x : x[1], reverse = True)
         return [sorted_scores[i] for i in range(3)]
         
@@ -56,6 +75,7 @@ class Vertex:
         self.position_score = 0
         self.propernoun_score = 0
         self.score = 0
+        self.tfidf_score = 0
         
         #sentence features
         self.tokens = nltk.word_tokenize(sentence)
@@ -66,7 +86,29 @@ class Vertex:
         self.position = position #get sentence position in the document
         self.num_numerical_tokens = len([token for token in self.tokens if token.isdigit()]) #get the number of numerical tokens in sentence
 
+    def calculate_tfidf_score(self, tfidf_dict) -> None:
+        self.tfidf_score = sum(tfidf_dict.get(word, 0) for word in self.tokens)
+        
+
     def score_vertex(self, max_sentence_length: int, num_sentences: int) -> int:
+
+        self.length_score = self.length/max_sentence_length if max_sentence_length!=0 else 0
+
+        if (self.position==0 or self.position==num_sentences-1):
+            self.position_score = 1
+        else:    
+            self.position_score = (num_sentences-self.position)/num_sentences if num_sentences!=0 else 0
+
+        self.propernoun_score = self.num_propernouns/self.length if self.length!=0  else 0
+        self.numerical_token_score = self.num_numerical_tokens/self.length if self.length!=0 else 0
+
+        self.score = (self.length_score+self.position_score+self.propernoun_score+self.numerical_token_score)*self.centrality
+
+        return self.score
+    
+    
+    #same as score_vertex, but considers td-idf scores of sentneces
+    def score_vertex_2(self, max_sentence_length: int, num_sentences: int) -> int:
 
         self.length_score = self.length/max_sentence_length if max_sentence_length!=0 else 0
 
@@ -85,8 +127,6 @@ class Vertex:
 
 #testing
 if __name__ == "__main__":
-
-    
     document = 'Technology is evolving at an unprecedented pace, reshaping industries and societies worldwide. Artificial intelligence, in particular, has transformed how we work, communicate, and solve problems. However, this rapid advancement also raises ethical questions about privacy, bias, and accountability. As we embrace innovation, it is crucial to address these challenges responsibly.'
     g = Graph(document=document)
     print(g.summarize())
